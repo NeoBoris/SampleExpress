704a2bbbd5c5e6d867a5baaab25c43cfb69dc1a3
kido <neoboris12@gmail.com>
2016-07-02 20:08:51 +0900

    jshintでの警告修正

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,8 +1,8 @@
 $(function() {
     var $main = $("#main");
-    var years = $main.data('years')
-    var expenses = $main.data('expenses')
-    var expensesByYear = new Array();
+    var years = $main.data('years');
+    var expenses = $main.data('expenses');
+    var expensesByYear = [];
     for (var i = 0; i < years.length; i++) {
         expensesByYear[i] = new Array(years[i], expenses[i]);
     }
