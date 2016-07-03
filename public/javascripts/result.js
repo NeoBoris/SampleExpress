2ff2b1d46bf5a39efcedaeed3a9ac38522ed3c20
kido <neoboris12@gmail.com>
2016-07-03 22:47:39 +0900

    グラフをjqplotからplotlyに変更

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -2,14 +2,25 @@ $(function() {
     var $main = $("#main");
     var years = $main.data('years');
     var expenses = $main.data('expenses');
-    var expensesByYear = [];
-    for (var i = 0; i < years.length; i++) {
-        expensesByYear[i] = new Array(years[i], expenses[i]);
-    }
-    jQuery . jqplot(
-        'jqPlot-sample',
-        [
-            expensesByYear
-        ]
-    );
+
+    var trace = {
+        x: years,
+        y: expenses,
+        type: 'lines+markers'
+    };
+
+    var layout = {
+        title:'家賃総支払い額',
+        xaxis: {
+            title: '年齢'
+        },
+        yaxis: {
+            title: '家賃総支払い額'
+        },
+        height: 400,
+        width: 480
+    };
+
+    var data = [trace];
+    Plotly.newPlot('myDiv', data, layout);
 });
