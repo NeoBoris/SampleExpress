59cf43abd183c9b1e3db82bda55873922b5fde6f
Eiichi Takamori <eiichit373@gmail.com.nospam>
2016-07-06 00:59:56 +0900

    元利均等返済の計算を追加。

--- test/test_calc.js
+++ test/test_calc.js
@@ -1,9 +1,19 @@
 var calc = require('../lib/calc');
 var should = require('chai').should();
+var expect = require('chai').expect;
 
 describe('calc', function() {
     it('指定した家賃の合計を計算する', function() {
         calc.getRentExpense(30000, 5).should.equal(1800000);
         calc.getRentExpense(80000, 45).should.equal(43200000);
     });
+});
+
+describe('EqualPaymentsWithInterest', function() {
+  it('元利均等返済を計算する', function() {
+    var info_list = calc.getEqualPaymentsWithInterest (30000000, 0.02, 30);
+    info_list.should.have.lengthOf(30 * 12);
+    Math.abs (30000000 - info_list[0].principal).should.be.below (0.01);
+    info_list[30 * 12 - 1].principal.should.equal (info_list[30 * 12 - 1].principal_repayment);
+  });
 });
