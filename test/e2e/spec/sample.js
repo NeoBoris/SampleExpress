c6b747606381efb8ae9aabe12a3e56ccab60b707
kido <neoboris12@gmail.com>
2016-08-04 00:28:09 +0900

    賃貸に更新料を追加する（受け入れテストの追加）

--- test/e2e/spec/sample.js
+++ test/e2e/spec/sample.js
@@ -44,4 +44,47 @@ describe('受け入れテストサンプル', function() {
           expect(result.element(by.binding('result.totalPayment')).getText()).toBe(totalPayments[i]);
       }
   });
+
+  it('賃貸を1つ追加し、2年に一度2ヶ月分の更新料ありで他の値は初期値のまま送信した結果の確認', function() {
+      // 結果(グラフ)が表示されていない
+      expect(element(by.id('paymentGraphArea')).isDisplayed()).toBeFalsy();
+
+      // 結果(テーブル)が表示されていない
+      expect(element(by.id('paymentTableArea')).isDisplayed()).toBeFalsy();
+
+      // 賃貸追加
+      element(by.id('typeAddButton')).click();
+
+      // 賃貸が追加される
+      // 保留
+
+      // 更新料の有無にチェックを入れる
+
+      // 更新料の値を2ヶ月分に設定
+
+      // 更新料の間隔を2年に設定
+
+      // 送信
+      element(by.id('sendButton')).click();
+
+      // 送信後少し待つ
+      browser.sleep(1000);
+
+      // 結果(グラフ)が表示される
+      expect(element(by.id('paymentGraphArea')).isDisplayed()).toBeTruthy();
+
+      // 結果(テーブル)が表示される
+      expect(element(by.id('paymentTableArea')).isDisplayed()).toBeTruthy();
+
+      // 結果確認
+      var ages = [ '31', '32', '33', '34', '35' ];
+      var yearPayments = [ '960000', '1120000', '960000', '1120000', '960000' ];
+      var totalPayments = [ '960000', '2080000', '3040000', '4160000', '5120000' ];
+      for (var i = 0; i < 5; i++) {
+          var result = element.all(by.repeater('result in results')).get(i);
+          expect(result.element(by.binding('result.age')).getText()).toBe(ages[i]);
+          expect(result.element(by.binding('result.yearPayment')).getText()).toBe(yearPayments[i]);
+          expect(result.element(by.binding('result.totalPayment')).getText()).toBe(totalPayments[i]);
+      }
+  });
 });
